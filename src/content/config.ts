dbe64fec5c16154cd5a4704715b6db3b8a823a16
bennetrr <76742318+bennetrr@users.noreply.github.com>
2023-09-30 17:26:40 +0200

    .

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,27 +1,32 @@
 import { defineCollection, z } from 'astro:content';
-export const collections = {
-    links: defineCollection({
-        type: 'data',
-        schema: z.object({
-            index: z.number().int().nonnegative(),
-            title: z.string(),
-            icon: z.string(),
-            link: z.string().url()
-        })
-    }),
-    projects: defineCollection({
-        type: 'content',
-        schema: z.object({
-            index: z.number().int().nonnegative(),
-            title: z.string(),
-            description: z.string(),
-            technologies: z.array(z.string()),
-            link: z.string().optional(),
-            links: z.array(z.object({
-                name: z.string(),
-                url: z.string().url(),
-                icon: z.string().optional()
-            }))
-        })
+
+const linkCollection = defineCollection({
+    type: 'data',
+    schema: z.object({
+        index: z.number().int().nonnegative(),
+        title: z.string(),
+        icon: z.string(),
+        link: z.string().url()
     })
+});
+
+const projectCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        index: z.number().int().nonnegative(),
+        title: z.string(),
+        description: z.string(),
+        technologies: z.array(z.string()),
+        link: z.string().optional(),
+        links: z.array(z.object({
+            name: z.string(),
+            url: z.string().url(),
+            icon: z.string().optional()
+        }))
+    })
+});
+
+export const collections = {
+    'links': linkCollection,
+    'projects': projectCollection
 };
