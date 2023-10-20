55fabc4e9e7de152dea998ad7ff66f8aca6a1a23
bennetrr <bennet@bennetr.me>
2023-10-20 11:07:34 +0200

    Adjusted Theme and code blocks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-    	require('@tailwindcss/typography'),
-	],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            code: {
+              backgroundColor: theme('colors.stone.800'),
+              padding: '0.2rem 0.25rem',
+              fontWeight: '400',
+              'border-radius': '0.25rem'
+            },
+            'code::before': {
+              content: '""'
+            },
+            'code::after': {
+              content: '""'
+            },
+            pre: {
+              backgroundColor: `${theme('colors.stone.800')} !important`
+            }
+          }
+        }
+      })
+    }
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
+  ]
+};
